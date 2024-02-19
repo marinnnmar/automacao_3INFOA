08da7b7f6fdeb37c25180203c6c550a3f8e7197a
Mariana Marcílio <mariana.marcilio@alunos.ifsuldeminas.edu.br>
2024-02-19 10:14:18 -0300

    Aula da semana 1

---
+++ semana1/eemplo2_entrada.py
@@ -0,0 +1,5 @@
+#entrada
+n1 = input("Digite o número 1: ")
+n2 = input("Digite o número 2: ")
+#saída
+print("Você digitou", n1, n2)
---
+++ semana1/exemplo3_processamento.py
@@ -0,0 +1,17 @@
+#entrada
+n1 = input("Digite o número 1: ")
+n2 = input("Digite o número 2: ")
+
+#processamento
+soma = int(n1) + int(n2)
+
+#saída
+print("A soma é de ", n1, " + ", n2, " = ", soma)
+
+#int("10") -> converte o texto "10" para inteiro 10
+#float("1.5") -> converte o texto "1.5" para real 1.5
+#str(10) -> converte o inteiro 10 para texto "10"
+
+#exponencial -> ** ---> 10 ** 2 = 100
+#resto da divisão -> % ---> 5 % 2 = 1
+#divisão inteira -> // ---> 5 // 2 = 2
